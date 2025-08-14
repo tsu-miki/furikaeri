507bdcae0b47ee7a470258d907e0331819841838
tsu-miki <shuta.tsuzuki@uzabase.com>
2025-08-14 23:22:51 +0900

    Update index title and enhance App component with a form for user input, including a submit handler.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 
 function App() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    
+    console.log("good")
+  } 
+
   return (
-    <div>Hello</div>
+    <>
+      <h1>ふりかえり</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="good">良かったこと</label>
+        <textarea id="good" name="good" />
+        <button type="submit">保存</button>
+      </form>
+    </>
   )
 }
 
