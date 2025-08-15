fa980db2c3bbd689f3262c112591fa4433724b3e
tsu-miki <wimps.r0930@icloud.com>
2025-08-15 20:10:21 +0900

    Enhance App component to manage textarea input state and update submit handler to log user input.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react"
 
 function App() {
+  const [good, setGood] = useState('');
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setGood('')
     
-    console.log("good")
+    console.log(good)
   } 
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setGood(e.target.value);
+  }
 
   return (
     <>
       <h1>ふりかえり</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="good">良かったこと</label>
-        <textarea id="good" name="good" />
+        <textarea id="good" name="good" onChange={handleChange} value={good} />
         <button type="submit">保存</button>
       </form>
     </>
